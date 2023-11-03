44d01006b3580decf224e197251b88dfe1a1c6a7
hariom <hk524569@gmail.com>
2023-11-03 12:05:38 +0530

    11/3/2023

---
+++ Array/Array1.js
@@ -0,0 +1,39 @@
+// array
+
+const myArr = [0, 1, 2, 3, 4, 5]
+const myHeors = ["shaktiman", "naagraj"]
+
+const myArr2 = new Array(1, 2, 3, 4)
+// console.log(myArr[1]);
+
+// Array methods
+
+// myArr.push(6)
+// myArr.push(7)
+// myArr.pop()
+
+// myArr.unshift(9)
+// myArr.shift()
+
+// console.log(myArr.includes(9));
+// console.log(myArr.indexOf(3));
+
+// const newArr = myArr.join()
+
+// console.log(myArr);
+// console.log( newArr);
+
+
+// slice, splice
+
+console.log("A ", myArr);
+
+const myn1 = myArr.slice(1, 3) //start to end-1
+
+console.log(myn1);
+console.log("B ", myArr);//it will not effect the original array
+
+
+const myn2 = myArr.splice(1, 3)
+console.log("C ", myArr);//it will change the original array
+console.log(myn2); //start it end
---
+++ Math/math1.js
@@ -0,0 +1,42 @@
+const score = 400
+// console.log(score);
+
+//created object of Number data type
+const balance = new Number(100)
+// console.log(balance);
+// console.log(typeof balance)//object
+
+
+// console.log(balance.toString().length);
+// console.log(balance.toFixed(1));//it will show one number after decimal value
+
+const otherNumber = 123.8966
+
+// console.log(otherNumber.toPrecision(4));//123.8
+
+const hundreds = 1000000
+// console.log(hundreds.toLocaleString('en-IN'));//10,00,000
+
+// +++++++++++++ Maths +++++++++++++++++++++++++++++
+
+// console.log(Math);
+// console.log(Math.abs(-4));//4
+// console.log(Math.round(4.6));  //5
+// console.log(Math.ceil(4.2));   //5(higer number)
+// console.log(Math.floor(4.9));  //4 (lower number)
+// console.log(Math.min(4, 3, 6, 8));  //3
+// console.log(Math.max(4, 3, 6, 8));  //8
+
+// console.log(Math.random()); //random number
+// console.log((Math.random()*10) + 1); //1 to 10
+// console.log(Math.floor(Math.random()*10) + 1);
+
+// const min = 10
+// const max = 20
+
+// console.log(Math.floor(Math.random() * (max - min + 1)) + min)
+
+let num=10
+console.log(Math.sqrt(num))
+
+//january 1,1970,UTC(the apoch)
