a7e5561739c30782e5399b81df5ccf5d2b1d119f
hariom <hk524569@gmail.com>
2024-01-08 15:04:13 +0530

    prototype over

--- Opps/prototypes2.js
+++ Opps/prototypes2.js
@@ -17,6 +17,44 @@ Object.prototype.printname=function(){
 heroPower.printname()
 
 myHeros.printname()
+//inheritance
 
+const Adity={
+    name:"college",
+    email:"college@gmail.com"
+}
+const Teacher={
+    makevideo:true
+}
+const TeacherSupport={
+    isAvailable:false
+}
+const TASupport={
+    makeAssignement:'js assignment',
+    fullTime:true,
+    //prototypal inheritance 
+    //accessing property of TeacherSupport class
+    __proto__:TeacherSupport
+}
+// Teacher.__proto__ = Aditya
+
+console.log(Adity)
+console.log(TASupport.isAvailable)
+
+//modern syntax
+//nheriting the properties of Aditya into Teacher
+Object.setPrototypeOf(Teacher,Adity)
+
+console.log(Teacher.name)
+
+//
+let anotheruserName="chaiorcode   "
+String.prototype.trueLength=function(){
+    console.log(this.trim())
+    console.log(this.trim().length)
+}
+anotheruserName.trueLength()
+"hariom".trueLength()
 /* array  ->object ->null
+
 */
